072cd4a1bc8c06067cbef11680210f9c5777d7ce
ushasandireddy <usharanisandireddy@gmail.com>
2024-06-07 11:42:14 +0530

    added program

---
+++ conditional.py
@@ -0,0 +1,18 @@
+#a=2
+#b=23
+#if a>b:
+#    print('false')
+#elif a<b:
+#    print('true')
+#else:
+#    print('true')
+
+
+
+x=20
+if x>25:
+    print("if cond is true")
+elif x<25:
+    print("cond is true")
+else:
+    print("if cond is false")
