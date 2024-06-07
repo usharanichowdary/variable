5a0316ed3683f12e3f94d72649c1a6a5803df7f8
ushasandireddy <usharanisandireddy@gmail.com>
2024-06-07 14:54:59 +0530

    added examples

--- conditional.py
+++ conditional.py
@@ -1,11 +1,11 @@
-#a=2
-#b=23
-#if a>b:
-#    print('false')
-#elif a<b:
-#    print('true')
-#else:
-#    print('true')
+a=2
+b=23
+if a>b:
+   print('false')
+elif a<b:
+   print('true')
+else:
+   print('true')
 
 
 
@@ -16,3 +16,36 @@ elif x<25:
     print("cond is true")
 else:
     print("if cond is false")
+
+
+
+
+#By nested loop
+x = 10
+y = 20
+
+if x > 5:
+    print("x is greater than 5")
+    if y > 15:
+        print("y is also greater than 15")
+    else:
+        print("y is not greater than 15")
+else:
+    print("x is not greater than 5")
+
+
+#By using logical operators
+
+x=10
+y=20
+z=30
+
+if x>5:
+    if y>15 and z>25:
+        print('x is greater than 5,y is greater than 15,z is greater than 25')
+    elif y<15 and z<=25:
+        print('x is greater than 5,y is lesser than 15,z is lessere than or equal to 25')
+    else:
+        print('x is greater than 5, y is lesser than 15')
+else:
+    print('x is lesser than 5')
