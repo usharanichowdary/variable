e6e8d94a9350653d70e443643b487ff33605e204
ushasandireddy <usharanisandireddy@gmail.com>
2024-06-20 21:34:46 +0530

    connected

--- mysql.py
+++ mysql.py
@@ -1,92 +1,110 @@
-import pymysql.cursors
+# import pymysql.cursors
 
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             database='sql learning',
-                             cursorclass=pymysql.cursors.DictCursor)
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='',
+#                              database='sql learning',
+#                              cursorclass=pymysql.cursors.DictCursor)
 
-try:
-       with connection:
-        with connection.cursor() as cursor:
-            sql = "INSERT INTO `fruits` (`colour`, `name`,`price`,`exp_date`) VALUES (%s,%s,%s,%s)"
-            cursor.execute(sql, ('green', 'grapes',87,'2024-06-05'))
-        connection.commit()
+# try:
+#        with connection:
+#         with connection.cursor() as cursor:
+#             sql = "INSERT INTO `fruits` (`colour`, `name`,`price`,`exp_date`) VALUES (%s,%s,%s,%s)"
+#             cursor.execute(sql, ('green', 'grapes',87,'2024-06-05'))
+#         connection.commit()
 
-except pymysql.error as e:
-    print(f"error: {e}")
+# except pymysql.error as e:
+#     print(f"error: {e}")
 
-finally:
-    connection.close()
+# finally:
+#     connection.close()
     
 
-import pymysql.cursors
+# import pymysql.cursors
 
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             database='sql learning',
-                             cursorclass=pymysql.cursors.DictCursor)
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='',
+#                              database='sql learning',
+#                              cursorclass=pymysql.cursors.DictCursor)
 
 
-with connection:
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO`fruits` (`colour`, `name`,`price`,`exp_date`) VALUES (%s,%s,%s,%s)"
-        cursor.execute(sql, ('blue', 'berry',89,'2024-05-06'))
-        connection.commit()
+# with connection:
+#     with connection.cursor() as cursor:
+#         sql = "INSERT INTO`fruits` (`colour`, `name`,`price`,`exp_date`) VALUES (%s,%s,%s,%s)"
+#         cursor.execute(sql, ('blue', 'berry',89,'2024-05-06'))
+#         connection.commit()
 
 
 
 
-#student table
-import pymysql.cursors
+# #student table
+# import pymysql.cursors
 
 
-connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='sql learning',
-    cursorclass=pymysql.cursors.DictCursor
-)
+# connection = pymysql.connect(
+#     host='localhost',
+#     user='root',
+#     password='',
+#     database='sql learning',
+#     cursorclass=pymysql.cursors.DictCursor
+# )
 
-try:
-    with connection.cursor() as cursor:
+# try:
+#     with connection.cursor() as cursor:
         
-        create_table_query = """
-        CREATE TABLE student (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(50),
-            age INT,
-            grade VARCHAR(10)
-        )
-        """
+#         create_table_query = """
+#         CREATE TABLE student (
+#             id INT AUTO_INCREMENT PRIMARY KEY,
+#             name VARCHAR(50),
+#             age INT,
+#             grade VARCHAR(10)
+#         )
+#         """
        
-        cursor.execute(create_table_query)
+#         cursor.execute(create_table_query)
    
-    connection.commit()
-finally:
-    connection.close()
+#     connection.commit()
+# finally:
+#     connection.close()
 
 
-#for inserting student data
+# #for inserting student data
 
-import pymysql.cursors
-
-
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             database='sql learning',
-                             cursorclass=pymysql.cursors.DictCursor)
+# import pymysql.cursors
 
 
-with connection:
-    with connection.cursor() as cursor:
-        sql = "INSERT INTO`student` (`id`, `name`,`age`,`grade`) VALUES (%s,%s,%s,%s)"
-        cursor.execute(sql, (4, 'chinna',26,'C'))
-        cursor.execute(sql, (5, 'leela',29,'D'))
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='',
+#                              database='sql learning',
+#                              cursorclass=pymysql.cursors.DictCursor)
 
-        connection.commit()
+
+# with connection:
+#     with connection.cursor() as cursor:
+#         sql = "INSERT INTO`student` (`id`, `name`,`age`,`grade`) VALUES (%s,%s,%s,%s)"
+#         cursor.execute(sql, (4, 'chinna',26,'C'))
+#         cursor.execute(sql, (5, 'leela',29,'D'))
+
+#         connection.commit()
+
+# import pymysql.cursors
+
+
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='',
+#                              database='sql learning',
+#                              cursorclass=pymysql.cursors.DictCursor)
+
+
+# with connection:
+#     with connection.cursor() as cursor:
+#         sql = "UPDATE `student` SET `age`=%s WHERE 'name'= %s"
+#         cursor.execute(sql,(35,'chinna'))
+        
+
+#         connection.commit()
